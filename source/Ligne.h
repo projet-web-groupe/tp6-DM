b8224d4a7e47ca55573c0ba686248bc519b5bcd5
Roxog <gabrielcano@hotmail.fr>
2017-10-16 10:06:41 +0200

    Gabi : ajout methode d'affichage (imcomplete) à la classe Ligne

--- source/Ligne.h
+++ source/Ligne.h
@@ -8,8 +8,15 @@ class Ligne {
 	T typeTransports;
 	Terminal origin;
 	Terminal destination;
-	public:
-		
+	int nbPassagerJour;
+public:
+	void affiche(void)const;
+
 };
+
+template<T>
+void Ligne::affiche(void)const{
+	std::cout << "Object : Ligne\n\tType de transport :" << typeTransports.getNom() << "\n\torigin : " << "recup origin" << "\n\tdestination : " << "recup dest" << "\n\tNombre de passager/jour : " << nbPassagerJour << std::endl;
+}
 
 #endif
