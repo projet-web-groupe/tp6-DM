d072befaea89f25da8cef273f796baace9b2667b
Roxog <gabrielcano@hotmail.fr>
2017-11-03 10:07:49 +0100

    a

--- source/Voyage.h
+++ source/Voyage.h
@@ -1,11 +1,16 @@
 #ifndef VOYAGE_H
 #define VOYAGE_H
+#include "Ligne.h"
+class Moyens;
 
 class Voyage {
 	Moyens origin;
 	Moyens destination;
 	//pointeur car destruction d'un voyage != destruction 
-	std::list<Ligne *> correspondance;
+	std::list<Ligne<Moyens>> correspondance;
+public:
+	Voyage();
+	~Voyage();
 }
 
 #endif
